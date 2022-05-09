7e551186d8a674bcc3675e5207f7f3268e3ee4e7
Juan2579 <juandavidgualdronnavarro@gmail.com>
2022-05-08 22:30:12 -0500

    Square variables and triangle variables

---
+++ figuras.js
@@ -0,0 +1,19 @@
+//Codigo del cuadrado
+console.group("Cuadrados")
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado mide: " + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es: " + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado ** 2;
+console.log("El area del cuadrado es: " + areaCuadrado + "cm2")
+console.groupEnd();
+
+
+//Codigo del triangulo
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+console.log("Los lados del triangulo miden: " + ladoTriangulo1 + "cm, " + ladoTriangulo2 + "cm, " + baseTriangulo + "cm")
