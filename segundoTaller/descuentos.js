2bc85c253f24f048f77904c25f2bef4f6da7dbd9
Juan2579 <juandavidgualdronnavarro@gmail.com>
2022-05-11 12:24:37 -0500

    Second workshop with desktop and mobile design

--- segundoTaller/descuentos.js
+++ segundoTaller/descuentos.js
@@ -5,3 +5,17 @@ function calcularPrecioConDescuento(precio, descuento) {
     return precioConDescuento;
 }
 
+function priceWithDiscount(){
+    const inputPrice = document.getElementById("inputPrice");
+    const priceValue = inputPrice.value
+
+
+    const inputDiscount = document.getElementById("inputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue)
+
+    const resultP = document.getElementById("priceP");
+
+    resultP.innerText = "El precio con descuento es de " + "$" + precioConDescuento
+}
