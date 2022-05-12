658843d380ac1e1d29213f955d5037a600b46c86
Juan2579 <juandavidgualdronnavarro@gmail.com>
2022-05-12 17:26:26 -0500

    Average exercise

--- tercerTaller/promedio.js
+++ tercerTaller/promedio.js
@@ -1,11 +1,37 @@
-const lista1 = [
-    100,
-    200,
-    300,
-    500
-]
+//Input que indica cuantos datos se ingresaran
+let inputNumbers = document.getElementById("inputNumbers");
 
+//Vamos a traer el contenedor donde pedimos los datos
+let cardInfo = document.querySelector('.card_info')
 
+//Vamos a traer los dos nuevos contenedores, el primero el contenedor que tendra todo lo que agreguemos y el segundo será el contenedor donde se agregaran los inputs nuevos 
+
+let cardResults = document.querySelector('.card_results')
+let newInputs = document.querySelector('.card_newInputs')
+let cardPrintResult = document.querySelector('.card_printResult')
+
+function mostrarDatos(){
+    cardInfo.style.display = 'none';
+    cardResults.style.display = 'flex';
+
+    for(let i = 0; i < inputNumbers.value; i++){
+        newInputs.innerHTML += `<input class="" id="" type="number">`;
+    }
+
+}
+
+function resultadoMediaAritmetica(){
+    const listaInputs = document.querySelectorAll('.card_results .card_newInputs input')
+
+    let lista = [];
+
+    for (let i = 0; i < listaInputs.length; i++) {
+        lista.push(Number(listaInputs[i].value));
+    }
+
+    cardPrintResult.innerText = "El promedio es de " + calcularMediaAritmetica(lista); 
+
+}
 
 
 
@@ -14,7 +40,15 @@ function calcularMediaAritmetica(lista){
     // for (let i = 0; i < lista.length; i++){
     // sumaLista = sumaLista + lista[i];
     // }
+
+    
+
+    const sumaLista = lista.reduce(
+        function(valorAcumulado = 0, nuevoElemento){
+            return valorAcumulado + nuevoElemento;
+
+    })
     const promedioLista = sumaLista / lista.length;
 
-    return promedioLista
+    return promedioLista;
 } 
