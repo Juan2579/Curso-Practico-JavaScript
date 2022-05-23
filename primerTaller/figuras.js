dd6f1b153c4a31f3ea51492bfc2d5a4f81ecf584
Juan2579 <juandavidgualdronnavarro@gmail.com>
2022-05-23 00:35:57 -0500

    Upgraded geometry page to be connected with the index

--- primerTaller/figuras.js
+++ primerTaller/figuras.js
@@ -1,3 +1,20 @@
+const burgerButton = document.querySelector('.button_burger')
+const xButton = document.querySelector('.button_x')
+const mobileNavigation = document.querySelector('.header_mobile-navigation')
+function appearNavigation() {
+    burgerButton.style.display = 'none'
+    xButton.style.display = 'block'
+    mobileNavigation.style.display = 'block'
+
+}
+function dissapearNavigation() {
+        burgerButton.style.display = 'block'
+        xButton.style.display = 'none'
+        mobileNavigation.style.display = 'none'
+    
+}
+
+
 //Codigo del cuadrado
 function perimetroCuadrado(lado){
     return lado* 4;
